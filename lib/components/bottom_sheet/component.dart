a7f739382993d4e4185df158067069d3c6d419de
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-10-14 01:16:37 +0300

    feat(ui): bottom sheet component

---
+++ lib/components/bottom_sheet/component.dart
@@ -0,0 +1,118 @@
+import "package:figma_squircle/figma_squircle.dart";
+import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:mony_app/app.dart";
+
+class BottomSheetComponent extends StatelessWidget {
+  final EdgeInsets viewPaddings;
+  final WidgetBuilder builder;
+
+  const BottomSheetComponent({
+    super.key,
+    required this.viewPaddings,
+    required this.builder,
+  });
+
+  static Future<T?> show<T>(
+    BuildContext context, {
+    required WidgetBuilder builder,
+  }) {
+    final navigator = appNavigatorKey.currentState;
+    if (navigator == null) return Future.value();
+    final theme = Theme.of(context);
+    final viewPaddings = MediaQuery.viewPaddingOf(context);
+
+    return navigator.push(
+      ModalBottomSheetRoute<T>(
+        builder: (context) {
+          return Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              BottomSheetComponent(
+                viewPaddings: viewPaddings,
+                builder: builder,
+              ),
+            ],
+          );
+        },
+        useSafeArea: true,
+        isScrollControlled: true,
+        modalBarrierColor: theme.colorScheme.scrim.withOpacity(0.3),
+        backgroundColor: Colors.transparent,
+        showDragHandle: false,
+        capturedThemes: InheritedTheme.capture(
+          from: context,
+          to: navigator.context,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Color.lerp(
+          theme.colorScheme.surfaceContainer,
+          theme.colorScheme.surface,
+          0.7,
+        ),
+        borderRadius: SmoothBorderRadius.all(
+          SmoothRadius(
+            cornerRadius: 26.r,
+            cornerSmoothing: 1.0,
+          ),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // -> handle
+          const _BottomSheetHandleComponent(),
+
+          // -> content
+          Flexible(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.only(bottom: viewPaddings.bottom + 8.h),
+              child: builder(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomSheetHandleComponent extends StatelessWidget {
+  const _BottomSheetHandleComponent();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 8.h),
+      child: SizedBox.fromSize(
+        size: Size.fromHeight(24.h),
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: SizedBox(
+            width: 46.w,
+            height: 4.h,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: theme.colorScheme.onSurfaceVariant.withOpacity(0.5),
+                borderRadius: SmoothBorderRadius.all(
+                  SmoothRadius(cornerRadius: 2.r, cornerSmoothing: 1.0),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,2 +1,4 @@
 export "./appbar/component.dart";
 export "./back_button/component.dart";
+export "./bottom_sheet/component.dart";
+export "./select/component.dart";
