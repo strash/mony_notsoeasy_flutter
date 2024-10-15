e0d68e12a9e6eca62cbb229e0b54b5918750fc88
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-10-15 16:58:56 +0300

    feat(migrations): m_1728991478_add_color_and_balance_columns_to_accounts

---
+++ lib/data/database/migrations/m_1728991478_add_color_and_balance_columns_to_accounts.dart
@@ -0,0 +1,25 @@
+import "package:mony_app/data/database/migration_service.dart";
+import "package:sqflite/sqflite.dart";
+
+final class M1728991478AddColorAndBalanceColumnsToAccounts
+    extends BaseMigration {
+  final _table = "accounts";
+
+  @override
+  Future<void> up(Database db) async {
+    final batch = db.batch();
+    batch.execute("ALTER TABLE $_table "
+        "ADD COLUMN color TEXT DEFAULT '0xFFFFFFFF' NOT NULL;");
+    batch.execute("ALTER TABLE $_table "
+        "ADD COLUMN balance REAL DEFAULT 0 NOT NULL;");
+    await batch.commit();
+  }
+
+  @override
+  Future<void> down(Database db) async {
+    final batch = db.batch();
+    batch.execute("ALTER TABLE $_table DROP COLUMN color;");
+    batch.execute("ALTER TABLE $_table DROP COLUMN balance;");
+    await batch.commit();
+  }
+}
