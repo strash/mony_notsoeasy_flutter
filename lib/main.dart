cc64e6a7bb717109f7484c657bed431b6308a60e
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-10-17 02:37:06 +0300

    chore: localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import "package:file_picker/file_picker.dart";
 import "package:flutter/material.dart";
 import "package:mony_app/app.dart";
 import "package:mony_app/app/app.dart";
 import "package:mony_app/data/database/database.dart";
+import "package:mony_app/data/filesystem/filesystem.dart";
 import "package:mony_app/domain/domain.dart";
 import "package:provider/provider.dart";
 
@@ -14,6 +16,18 @@ void main() async {
     AppEventServiceBuilder(
       child: MultiProvider(
         providers: [
+          // -> import/export service
+          Provider<ImportExportService>(
+            create: (context) {
+              return ImportExportService(
+                csvFilesystemRepository: CsvFilesystemRepository(
+                  filePicker: FilePicker.platform,
+                ),
+              );
+            },
+          ),
+
+          // -> account service
           Provider<AccountService>(
             create: (context) {
               return AccountService(
