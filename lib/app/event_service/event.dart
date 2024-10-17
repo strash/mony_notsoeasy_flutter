c4c0ec2bbb5936183850cdfe21243fad581c8029
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-10-18 02:37:33 +0300

    chore: refactoring

--- lib/app/event_service/event.dart
+++ lib/app/event_service/event.dart
@@ -1,7 +1,7 @@
 part of "./event_service.dart";
 
 @freezed
-class Event with _$Event {
+sealed class Event with _$Event {
   const factory Event.accountCreated({
     required Type sender,
     required AccountModel account,
