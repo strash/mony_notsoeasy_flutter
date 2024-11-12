4984494c28783552e378f30128c7e7683fc5866a
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-11-12 11:52:00 +0300

    fix(constants): decrease kMaxAmountLength because of double parsing

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -2,6 +2,6 @@ const kAmountPattern = r"^[-+]?(?:0|^[-+]?[1-9]\d*?)(?:[.,]\d{0,2})?$";
 
 const kDefaultCurrencyCode = "RUB";
 
-/// -1,000,000,000,000,000.00
-const kMaxAmountLength = 25;
+/// -1,000,000,000.00
+const kMaxAmountLength = 14;
 const kMaxTitleLength = 150;
