5cc2d57130caa7efddaa01dd898f2898751b5a30
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-11-12 19:22:19 +0300

    feat(new_transaction): animate keyboard on press

--- lib/features/new_transaction/components/keyboard_button.dart
+++ lib/features/new_transaction/components/keyboard_button.dart
@@ -4,9 +4,10 @@ import "package:flutter_screenutil/flutter_screenutil.dart";
 import "package:flutter_svg/svg.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:mony_app/app/use_case/use_case.dart";
+import "package:mony_app/common/common.dart";
 import "package:mony_app/features/new_transaction/components/components.dart";
 
-class NewTransactionSymbolButtonComponent extends StatelessWidget {
+class NewTransactionSymbolButtonComponent extends StatefulWidget {
   final ButtonType button;
   final String value;
   final UseCase<Future<void>, ButtonType> onTap;
@@ -19,65 +20,119 @@ class NewTransactionSymbolButtonComponent extends StatelessWidget {
   });
 
   @override
+  State<NewTransactionSymbolButtonComponent> createState() =>
+      _NewTransactionSymbolButtonComponentState();
+}
+
+class _NewTransactionSymbolButtonComponentState
+    extends State<NewTransactionSymbolButtonComponent>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  bool get _isEnabled => widget.button.isEnabled(widget.value);
+
+  void _animationStatusListener(AnimationStatus status) {
+    if (mounted && status == AnimationStatus.completed) {
+      _controller.reverse();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: Durations.short2);
+    _animation = Tween<double>(begin: .0, end: 1.0)
+        .chain(CurveTween(curve: Curves.easeInOut))
+        .animate(_controller);
+    _animation.addStatusListener(_animationStatusListener);
+  }
+
+  @override
+  void dispose() {
+    _animation.removeStatusListener(_animationStatusListener);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final isEnabled = button.isEnabled(value);
 
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
-      onTap: isEnabled ? () => onTap(context, button) : null,
-      child: AspectRatio(
-        aspectRatio: 1.618033,
-        child: TweenAnimationBuilder<Color?>(
-          duration: Durations.short4,
-          curve: Curves.easeInOut,
-          tween: ColorTween(
-            begin: button.color,
-            end: isEnabled
-                ? button.color
-                : theme.colorScheme.surfaceContainer.withOpacity(.8),
-          ),
-          child: Center(
-            child: AnimatedOpacity(
-              opacity: isEnabled ? 1.0 : .5,
-              duration: Durations.short4,
-              curve: Curves.easeInOut,
-              child: switch (button) {
-                final ButtonTypeSymbol button => Text(
-                    button.value,
-                    style: GoogleFonts.golosText(
-                      fontSize: 34.sp,
-                      fontWeight: FontWeight.w500,
-                      color: theme.colorScheme.onSurface,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                final ButtonTypeAction button => SvgPicture.asset(
-                    button.icon,
-                    width: 36.r,
-                    height: 36.r,
-                    colorFilter: ColorFilter.mode(
-                      theme.colorScheme.surface,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-              },
-            ),
-          ),
-          builder: (context, color, child) {
-            return DecoratedBox(
-              decoration: ShapeDecoration(
-                color: color,
-                shape: SmoothRectangleBorder(
-                  borderRadius: SmoothBorderRadius.all(
-                    SmoothRadius(cornerRadius: 20.r, cornerSmoothing: 1.0),
+      onTap: _isEnabled
+          ? () {
+              widget.onTap(context, widget.button);
+              _controller.forward();
+            }
+          : null,
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return Transform.scale(
+            scale: 1.0 - _animation.value.remap(.0, 1.0, .0, .07),
+            child: AspectRatio(
+              aspectRatio: 1.618033,
+              child: TweenAnimationBuilder<Color?>(
+                duration: Durations.short4,
+                curve: Curves.easeInOut,
+                tween: ColorTween(
+                  begin: widget.button.color,
+                  end: _isEnabled
+                      ? widget.button.color
+                      : theme.colorScheme.surfaceContainer.withOpacity(.8),
+                ),
+                child: Center(
+                  child: AnimatedOpacity(
+                    opacity: _isEnabled ? 1.0 : .5,
+                    duration: Durations.short4,
+                    curve: Curves.easeInOut,
+                    child: switch (widget.button) {
+                      final ButtonTypeSymbol button => Text(
+                          button.value,
+                          style: GoogleFonts.golosText(
+                            fontSize: 34.sp,
+                            fontWeight: FontWeight.w500,
+                            color: theme.colorScheme.onSurface,
+                            decoration: TextDecoration.none,
+                          ),
+                        ),
+                      final ButtonTypeAction button => SvgPicture.asset(
+                          button.icon,
+                          width: 36.r,
+                          height: 36.r,
+                          colorFilter: ColorFilter.mode(
+                            theme.colorScheme.surface,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                    },
                   ),
                 ),
+                builder: (context, color, child) {
+                  return ClipSmoothRect(
+                    radius: SmoothBorderRadius.all(
+                      SmoothRadius(
+                        cornerRadius: 20.r,
+                        cornerSmoothing: 1.0,
+                      ),
+                    ),
+                    child: ColoredBox(
+                      color: color ?? theme.colorScheme.surfaceContainer,
+                      child: ColoredBox(
+                        color: theme.colorScheme.primaryContainer.withOpacity(
+                          _animation.value.remap(.0, 1.0, .0, .2),
+                        ),
+                        child: child,
+                      ),
+                    ),
+                  );
+                },
               ),
-              child: child,
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
