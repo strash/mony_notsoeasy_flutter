6ed5076d7efa7e3782ad86ccef3e969e7860131d
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-11-23 18:52:24 +0300

    fix(feed): balance for all accounts doesn't updates when second account was created

--- lib/features/feed/use_case/on_app_state_changed/on_account_created.dart
+++ lib/features/feed/use_case/on_app_state_changed/on_account_created.dart
@@ -25,21 +25,24 @@ final class _OnAccountCreated {
       }),
     );
 
-    final balances = await accountSevrice.getBalances(ids: [account.id]);
-    viewModel.addPageScroll(viewModel.pages.length);
-    pages.add(
-      FeedPageStateSingleAccount(
-        scrollPage: 0,
-        canLoadMore: true,
-        feed: const [],
-        account: account.copyWith(),
-        balance: balances.first,
-      ),
-    );
+    {
+      final balances = await accountSevrice.getBalances(ids: [account.id]);
+      viewModel.addPageScroll(viewModel.pages.length);
+      pages.add(
+        FeedPageStateSingleAccount(
+          scrollPage: 0,
+          canLoadMore: true,
+          feed: const [],
+          account: account.copyWith(),
+          balance: balances.first,
+        ),
+      );
+    }
 
     if (pages.whereType<FeedPageStateAllAccounts>().isEmpty) {
       final accounts = await accountSevrice.getAll();
       final transactions = await transactionService.getMany(page: 0);
+      final balances = await accountSevrice.getBalances();
       viewModel.addPageScroll(0);
       final page = FeedPageStateAllAccounts(
         scrollPage: 1,
