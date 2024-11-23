2c96ebe672d3df71d6e9631a49e7386c69fa6de0
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-11-23 18:46:29 +0300

    fix(transaction): not closing window on deletion

--- lib/features/transaction/use_case/on_app_state_changed.dart
+++ lib/features/transaction/use_case/on_app_state_changed.dart
@@ -17,26 +17,30 @@ final class OnTransactionAppStateChanged
     switch (event) {
       case EventAccountCreated() || EventTransactionCreated():
         break;
+
       case EventAccountUpdated(value: final account):
-        if (viewModel.transaction.account.id != account.id) return;
+        if (account.id != viewModel.transaction.account.id) return;
         viewModel.setProtectedState(() {
           viewModel.transaction = viewModel.transaction.copyWith(
             account: account.copyWith(),
           );
         });
+
       case EventAccountDeleted(value: final account):
-        if (viewModel.transaction.account.id != account.id) return;
+        if (account.id != viewModel.transaction.account.id) return;
         navigator.pop<void>();
-      case EventTransactionUpdated(:final value):
-        if (value.id == viewModel.transaction.id) {
-          viewModel.setProtectedState(() {
-            viewModel.transaction = value.copyWith();
-          });
-        }
+
+      case EventTransactionUpdated(value: final transaction):
+        if (transaction.id != viewModel.transaction.id) return;
+        viewModel.setProtectedState(() {
+          viewModel.transaction = transaction.copyWith();
+        });
+
       // TODO: слушать app events на удаление категории и
       // закрывать экран, если удалена категория этой транзакции
+
       case EventTransactionDeleted(value: final transaction):
-        if (viewModel.transaction.account.id != transaction.id) return;
+        if (transaction.id != viewModel.transaction.id) return;
         navigator.pop<void>();
     }
   }
