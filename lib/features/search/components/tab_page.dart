28db5dd6599f122b4ee59ab775f7584ccca66160
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2025-01-11 21:44:47 +0300

    feat(search): add empty states

--- lib/features/search/components/tab_page.dart
+++ lib/features/search/components/tab_page.dart
@@ -18,11 +18,19 @@ class SearchTabPageComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     final viewModel = context.viewModel<SearchViewModel>();
     final onTransactionPressed = viewModel<OnTransactionPressed>();
     final onAccountPressed = viewModel<OnAccountPressed>();
     final onCategoryPressed = viewModel<OnCategoryPressed>();
     final onTagPressed = viewModel<OnTagPressed>();
+    final isEmpty = switch (tab) {
+      ESearchTab.transactions => viewModel.transactions.isEmpty,
+      ESearchTab.accounts => viewModel.accounts.isEmpty,
+      ESearchTab.categories => viewModel.categories.isEmpty,
+      ESearchTab.tags => viewModel.tags.isEmpty,
+    };
 
     return CustomScrollView(
       controller: viewModel.getPageTabController(tab),
@@ -33,130 +41,143 @@ class SearchTabPageComponent extends StatelessWidget {
         // -> top offset
         SliverToBoxAdapter(child: SizedBox(height: topOffset)),
 
+        // -> empty state
+        if (isEmpty)
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: topOffset),
+              child: FeedEmptyStateComponent(
+                color: theme.colorScheme.onSurface,
+              ),
+            ),
+          )
+
         // -> content
-        switch (tab) {
-          // TODO: добавить эмптистейты
-          // -> transactions
-          ESearchTab.transactions => SliverList.separated(
-              findChildIndexCallback: (key) {
-                final id = (key as ValueKey).value;
-                final index =
-                    viewModel.transactions.indexWhere((e) => e.id == id);
-                return index != -1 ? index : null;
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 25.0);
-              },
-              itemCount: viewModel.transactions.length,
-              itemBuilder: (context, index) {
-                final item = viewModel.transactions.elementAt(index);
+        else
+          switch (tab) {
+            // -> transactions
+            ESearchTab.transactions => SliverList.separated(
+                findChildIndexCallback: (key) {
+                  final id = (key as ValueKey).value;
+                  final index =
+                      viewModel.transactions.indexWhere((e) => e.id == id);
+                  return index != -1 ? index : null;
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 25.0);
+                },
+                itemCount: viewModel.transactions.length,
+                itemBuilder: (context, index) {
+                  final item = viewModel.transactions.elementAt(index);
 
-                return GestureDetector(
-                  key: ValueKey<String>(item.id),
-                  behavior: HitTestBehavior.opaque,
-                  onTap: () => onTransactionPressed(context, item),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: FeedItemComponent(
-                      transaction: item,
-                      showFullDate: true,
+                  return GestureDetector(
+                    key: ValueKey<String>(item.id),
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () => onTransactionPressed(context, item),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: FeedItemComponent(
+                        transaction: item,
+                        showFullDate: true,
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
+                  );
+                },
+              ),
 
-          // -> accounts
-          ESearchTab.accounts => SliverList.separated(
-              findChildIndexCallback: (key) {
-                final id = (key as ValueKey).value;
-                final index = viewModel.accounts.indexWhere((e) => e.id == id);
-                return index != -1 ? index : null;
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 25.0);
-              },
-              itemCount: viewModel.accounts.length,
-              itemBuilder: (context, index) {
-                final item = viewModel.accounts.elementAt(index);
+            // -> accounts
+            ESearchTab.accounts => SliverList.separated(
+                findChildIndexCallback: (key) {
+                  final id = (key as ValueKey).value;
+                  final index =
+                      viewModel.accounts.indexWhere((e) => e.id == id);
+                  return index != -1 ? index : null;
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 25.0);
+                },
+                itemCount: viewModel.accounts.length,
+                itemBuilder: (context, index) {
+                  final item = viewModel.accounts.elementAt(index);
 
-                return GestureDetector(
-                  key: ValueKey<String>(item.id),
-                  behavior: HitTestBehavior.opaque,
-                  onTap: () => onAccountPressed.call(context, item),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: AccountComponent(
-                      account: item,
-                      showCurrencyTag: true,
+                  return GestureDetector(
+                    key: ValueKey<String>(item.id),
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () => onAccountPressed.call(context, item),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: AccountComponent(
+                        account: item,
+                        showCurrencyTag: true,
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
+                  );
+                },
+              ),
 
-          // -> categories
-          ESearchTab.categories => SliverList.separated(
-              findChildIndexCallback: (key) {
-                final id = (key as ValueKey).value;
-                final index =
-                    viewModel.categories.indexWhere((e) => e.id == id);
-                return index != -1 ? index : null;
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 25.0);
-              },
-              itemCount: viewModel.categories.length,
-              itemBuilder: (context, index) {
-                final item = viewModel.categories.elementAt(index);
+            // -> categories
+            ESearchTab.categories => SliverList.separated(
+                findChildIndexCallback: (key) {
+                  final id = (key as ValueKey).value;
+                  final index =
+                      viewModel.categories.indexWhere((e) => e.id == id);
+                  return index != -1 ? index : null;
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 25.0);
+                },
+                itemCount: viewModel.categories.length,
+                itemBuilder: (context, index) {
+                  final item = viewModel.categories.elementAt(index);
 
-                return GestureDetector(
-                  key: ValueKey<String>(item.id),
-                  onTap: () => onCategoryPressed.call(context, item),
-                  behavior: HitTestBehavior.opaque,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: CategoryComponent(category: item),
-                  ),
-                );
-              },
-            ),
-
-          // -> tags
-          ESearchTab.tags => SliverList.separated(
-              findChildIndexCallback: (key) {
-                final id = (key as ValueKey).value;
-                final index = viewModel.tags.indexWhere((e) => e.id == id);
-                return index != -1 ? index : null;
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 25.0);
-              },
-              itemCount: viewModel.tags.length,
-              itemBuilder: (context, index) {
-                final item = viewModel.tags.elementAt(index);
-
-                return GestureDetector(
-                  key: ValueKey<String>(item.id),
-                  onTap: () => onTagPressed.call(context, item),
-                  behavior: HitTestBehavior.opaque,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Row(
-                      children: [
-                        Flexible(
-                          child: TagComponent(tag: item),
-                        ),
-                      ],
+                  return GestureDetector(
+                    key: ValueKey<String>(item.id),
+                    onTap: () => onCategoryPressed.call(context, item),
+                    behavior: HitTestBehavior.opaque,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: CategoryComponent(category: item),
                     ),
-                  ),
-                );
-              },
-            ),
-        },
+                  );
+                },
+              ),
+
+            // -> tags
+            ESearchTab.tags => SliverList.separated(
+                findChildIndexCallback: (key) {
+                  final id = (key as ValueKey).value;
+                  final index = viewModel.tags.indexWhere((e) => e.id == id);
+                  return index != -1 ? index : null;
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 25.0);
+                },
+                itemCount: viewModel.tags.length,
+                itemBuilder: (context, index) {
+                  final item = viewModel.tags.elementAt(index);
+
+                  return GestureDetector(
+                    key: ValueKey<String>(item.id),
+                    onTap: () => onTagPressed.call(context, item),
+                    behavior: HitTestBehavior.opaque,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: [
+                          Flexible(
+                            child: TagComponent(tag: item),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+          },
 
         // -> bottom offset
-        SliverToBoxAdapter(child: SizedBox(height: bottomOffset)),
+        if (!isEmpty) SliverToBoxAdapter(child: SizedBox(height: bottomOffset)),
       ],
     );
   }
