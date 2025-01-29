f1a9631473b738e8760b19951aa05f01758ee1e0
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2025-01-29 16:06:45 +0300

    feat(ui): context menu item now can be active and will show a check mark icon

--- lib/components/context_menu/item.dart
+++ lib/components/context_menu/item.dart
@@ -1,16 +1,20 @@
 import "package:flutter/material.dart";
+import "package:flutter_svg/svg.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:mony_app/components/components.dart";
+import "package:mony_app/gen/assets.gen.dart";
 
 class ContextMenuItemComponent extends StatelessWidget {
   final Widget label;
   final Widget? icon;
+  final bool? isActive;
   final VoidCallback onTap;
 
   const ContextMenuItemComponent({
     super.key,
     required this.label,
     this.icon,
+    this.isActive,
     required this.onTap,
   });
 
@@ -29,18 +33,47 @@ class ContextMenuItemComponent extends StatelessWidget {
         behavior: HitTestBehavior.opaque,
         onTap: onTap,
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(20.0, 12.0, 16.0, 12.0),
+          padding: EdgeInsets.fromLTRB(
+            isActive != null ? 10.0 : 20.0,
+            12.0,
+            16.0,
+            12.0,
+          ),
           child: SeparatedComponent.list(
             direction: Axis.horizontal,
             crossAxisAlignment: CrossAxisAlignment.center,
-            separatorBuilder: (context, index) => const SizedBox(width: 10.0),
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            separatorBuilder: (context, index) => const SizedBox(width: 10.0),
             children: [
-              // -> label
               Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 2.0),
-                  child: label,
+                child: Row(
+                  children: [
+                    // -> check icon
+                    if (isActive != null)
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        child: SizedBox.square(
+                          dimension: 26.0,
+                          child: isActive!
+                              ? SvgPicture.asset(
+                                  Assets.icons.checkmark,
+                                  width: 26.0,
+                                  height: 26.0,
+                                  colorFilter: ColorFilter.mode(
+                                    theme.colorScheme.primary,
+                                    BlendMode.srcIn,
+                                  ),
+                                )
+                              : null,
+                        ),
+                      ),
+
+                    // -> label
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 2.0),
+                      child: label,
+                    ),
+                  ],
                 ),
               ),
 
