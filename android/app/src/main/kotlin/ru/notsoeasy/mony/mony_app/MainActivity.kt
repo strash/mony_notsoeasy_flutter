9e8c0612df2dd60d41d0561e3b78088afa2cb8cc
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2025-02-06 16:30:16 +0500

    feat(android): add icons; implement review request

--- android/app/src/main/kotlin/ru/notsoeasy/mony/mony_app/MainActivity.kt
+++ android/app/src/main/kotlin/ru/notsoeasy/mony/mony_app/MainActivity.kt
@@ -1,5 +1,39 @@
 package ru.notsoeasy.mony.mony_app
 
+import com.google.android.play.core.review.ReviewManagerFactory
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+
+class MainActivity: FlutterActivity() {
+    private val channelName = "strash.mony/review"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName).setMethodCallHandler {
+                call, result ->
+            if (call.method == "requestReview") {
+                requestReview(result)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun requestReview(result: MethodChannel.Result): Unit {
+        val manager = ReviewManagerFactory.create(context)
+        val request = manager.requestReviewFlow()
+        request.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val reviewInfo = task.result
+                val flow = manager.launchReviewFlow(activity, reviewInfo)
+                flow.addOnCompleteListener { _ -> }
+                result.success(null)
+            } else {
+                result.error("SOME_SHIT", "Some shit happened", null)
+            }
+        }
+    }
+}
+
