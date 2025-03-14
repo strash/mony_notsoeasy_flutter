d16398b555f894ba1102ba79a5a0227a6e42a664
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2025-03-14 13:52:14 +0300

    feat(ui): add category with context menu component

---
+++ lib/components/category_with_context_menu/component.dart
@@ -0,0 +1,187 @@
+import "package:figma_squircle_updated/figma_squircle.dart";
+import "package:flutter/material.dart";
+import "package:flutter/services.dart" show HapticFeedback;
+import "package:flutter_svg/svg.dart";
+import "package:mony_app/app/descriptable/descriptable.dart";
+import "package:mony_app/app/use_case/use_case.dart";
+import "package:mony_app/common/extensions/double.dart";
+import "package:mony_app/components/category/component.dart";
+import "package:mony_app/components/category_with_context_menu/use_case/on_context_menu_selected.dart"
+    show TCategoryContextMenuValue;
+import "package:mony_app/components/context_menu/item.dart";
+import "package:mony_app/components/context_menu/menu.dart";
+import "package:mony_app/components/context_menu/separator.dart";
+import "package:mony_app/components/separated/component.dart";
+import "package:mony_app/domain/models/category.dart";
+import "package:mony_app/gen/assets.gen.dart";
+
+export "./use_case/use_case.dart";
+
+enum ECategoryContextMenuItem implements IDescriptable {
+  edit,
+  delete;
+
+  String get icon {
+    return switch (this) {
+      ECategoryContextMenuItem.edit => Assets.icons.pencil,
+      ECategoryContextMenuItem.delete => Assets.icons.trashFill,
+    };
+  }
+
+  @override
+  String get description {
+    return switch (this) {
+      ECategoryContextMenuItem.edit => "Редактировать",
+      ECategoryContextMenuItem.delete => "Удалить",
+    };
+  }
+}
+
+class CategoryWithContextMenuComponent extends StatefulWidget {
+  final CategoryModel category;
+  final bool isColorsVisible;
+  final UseCase<void, CategoryModel> onTap;
+  final UseCase<Future<void>, TCategoryContextMenuValue> onMenuSelected;
+
+  const CategoryWithContextMenuComponent({
+    super.key,
+    required this.category,
+    required this.isColorsVisible,
+    required this.onTap,
+    required this.onMenuSelected,
+  });
+
+  @override
+  State<CategoryWithContextMenuComponent> createState() =>
+      _CategoryWithContextMenuComponentState();
+}
+
+class _CategoryWithContextMenuComponentState
+    extends State<CategoryWithContextMenuComponent>
+    with SingleTickerProviderStateMixin {
+  late final _controller = AnimationController(
+    vsync: this,
+    duration: Durations.long2,
+  );
+
+  final _scale = 1.015;
+
+  void _onLongPress(VoidCallback activate) {
+    HapticFeedback.heavyImpact();
+    activate();
+    _controller.reset();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ContextMenuComponent(
+      offset: const Offset(10.0, 10.0),
+      shiftIntoBounds: true,
+      buttonBuilder: (context, isOpened, activate) {
+        return GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            widget.onTap(context, widget.category);
+          },
+          onLongPressDown: (_) {
+            _controller.forward();
+          },
+          onLongPressCancel: () {
+            _controller.reverse();
+          },
+          onLongPress: isOpened ? null : () => _onLongPress(activate),
+          child: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, _) {
+              final scale = _controller.value.remap(.0, 1.0, 1.0, _scale);
+
+              return Transform.scale(
+                scale: !isOpened ? scale : 1.0,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 10.0,
+                  ),
+                  child: Opacity(
+                    opacity: isOpened ? .0 : 1.0,
+                    child: CategoryComponent(
+                      category: widget.category,
+                      showColors: widget.isColorsVisible,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+      buttonProxyBuilder: (context, anim, dismiss) {
+        final theme = Theme.of(context);
+
+        return GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: dismiss,
+          child: Transform.scale(
+            scale: _scale,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: DecoratedBox(
+                decoration: ShapeDecoration(
+                  color: theme.colorScheme.surface,
+                  shape: const SmoothRectangleBorder(
+                    borderRadius: SmoothBorderRadius.all(
+                      SmoothRadius(cornerRadius: 23.0, cornerSmoothing: .6),
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: CategoryComponent(
+                    category: widget.category,
+                    showColors: widget.isColorsVisible,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      popupBuilder: (context, anim, dismiss) {
+        final theme = Theme.of(context);
+
+        return SeparatedComponent.builder(
+          mainAxisSize: MainAxisSize.min,
+          separatorBuilder: (context, index) {
+            return const ContextMenuSeparatorComponent(isBig: false);
+          },
+          itemCount: ECategoryContextMenuItem.values.length,
+          itemBuilder: (context, index) {
+            final item = ECategoryContextMenuItem.values.elementAt(index);
+
+            return ContextMenuItemComponent(
+              label: Text(item.description),
+              icon: SvgPicture.asset(
+                item.icon,
+                colorFilter: ColorFilter.mode(
+                  theme.colorScheme.onSurface,
+                  BlendMode.srcIn,
+                ),
+              ),
+              onTap: () {
+                final value = (menu: item, category: widget.category);
+                widget.onMenuSelected(context, value);
+                dismiss();
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
---
+++ lib/components/category_with_context_menu/use_case/on_context_menu_selected.dart
@@ -0,0 +1,87 @@
+import "package:flutter/widgets.dart" show BuildContext, Text;
+import "package:mony_app/app/event_service/event_service.dart";
+import "package:mony_app/app/theme/theme.dart";
+import "package:mony_app/app/use_case/use_case.dart";
+import "package:mony_app/common/extensions/build_context.dart";
+import "package:mony_app/components/alert/component.dart";
+import "package:mony_app/components/bottom_sheet/sheet.dart";
+import "package:mony_app/components/category_with_context_menu/component.dart";
+import "package:mony_app/domain/models/category.dart";
+import "package:mony_app/domain/services/database/category.dart";
+import "package:mony_app/domain/services/database/vo/category.dart";
+import "package:mony_app/domain/services/local_storage/shared_preferences.dart";
+import "package:mony_app/features/category_form/page/view_model.dart";
+import "package:provider/provider.dart";
+
+typedef TCategoryContextMenuValue =
+    ({ECategoryContextMenuItem menu, CategoryModel category});
+
+final class OnCategoryWithContextMenuSelected
+    extends UseCase<Future<void>, TCategoryContextMenuValue> {
+  @override
+  Future<void> call(
+    BuildContext context, [
+    TCategoryContextMenuValue? value,
+  ]) async {
+    if (value == null) throw ArgumentError.notNull();
+
+    final (:menu, :category) = value;
+    final categoryService = context.read<DomainCategoryService>();
+    final appService = context.viewModel<AppEventService>();
+    final sharedPrefService = context.read<DomainSharedPreferencesService>();
+    final shouldConfirm = await sharedPrefService.getSettingsConfirmCategory();
+
+    switch (menu) {
+      // -> edit
+      case ECategoryContextMenuItem.edit:
+        final result = await BottomSheetComponent.show<CategoryVO>(
+          // ignore: use_build_context_synchronously
+          context,
+          showDragHandle: false,
+          builder: (context, bottom) {
+            return CategoryFormPage(
+              keyboardHeight: bottom,
+              transactionType: category.transactionType,
+              category: CategoryVariantModel(model: category),
+              additionalUsedTitles: const [],
+            );
+          },
+        );
+        if (result == null) return;
+        final model = await categoryService.update(
+          model: category.copyWith(
+            title: result.title,
+            icon: result.icon,
+            colorName: EColorName.from(result.colorName),
+            transactionType: result.transactionType,
+          ),
+        );
+        appService.notify(EventCategoryUpdated(model));
+
+      // -> delete
+      case ECategoryContextMenuItem.delete:
+        final EAlertResult? result;
+        if (shouldConfirm) {
+          result = await AlertComponet.show(
+            // ignore: use_build_context_synchronously
+            context,
+            title: const Text("Удаление категории"),
+            description: const Text(
+              "Вместе с категорией будут удалены все транзакции, связанные с "
+              "этой категорией. Эту проверку можно отключить в настройках.",
+            ),
+          );
+        } else {
+          result = EAlertResult.ok;
+        }
+        if (result == null) return;
+        switch (result) {
+          case EAlertResult.cancel:
+            return;
+          case EAlertResult.ok:
+            await categoryService.delete(id: category.id);
+            appService.notify(EventCategoryDeleted(category));
+        }
+    }
+  }
+}
